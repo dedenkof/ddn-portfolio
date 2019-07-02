4eccd38fd2c139b80406357324cb003316c837e4
Volodymyr Dedenko <dedenkof@ukr.net>
2019-07-02 18:11:39 +0300

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ddn-portfolio
         </a>
       </header>
     </div>
