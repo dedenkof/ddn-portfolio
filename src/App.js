3fd077ea13e9c89b052b5954231611d8a783057d
Volodymyr Dedenko <dedenkof@ukr.net>
2019-07-08 21:58:30 +0300

    Add react-mdl and main menu component and future pages to my projects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Fragment } from 'react';
 //import React, { Component, Fragment } from 'react';
 import './App.css';
-
-
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 
 
 /*class App extends Component {
@@ -16,7 +15,28 @@ import './App.css';
 
 function App() {
   return (
-      <Fragment><h1>Hello world</h1></Fragment>
+      <Fragment>{/* Uses a transparent header that draws on top of the layout's background */}
+        <div style={{height: '100vh', position: 'relative'}}>
+          <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+            <Header transparent title="Title" style={{color: 'white'}}>
+              <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+              </Navigation>
+            </Header>
+            <Drawer title="Title">
+              <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+              </Navigation>
+            </Drawer>
+            <Content />
+          </Layout>
+        </div></Fragment>
   );
 }
 
