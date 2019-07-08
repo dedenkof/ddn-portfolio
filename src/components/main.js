e533a2248595e27366f6610b9ea9de06aea92ea6
Volodymyr Dedenko <dedenkof@ukr.net>
2019-07-09 00:15:56 +0300

    Change order item routing in main.js

---
+++ src/components/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Landing from './landing';
+import About from './about';
+import Resume from './resume';
+import Projects from './projects';
+import Blog from './blog';
+import Contact from './contact';
+
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+    </Switch>
+)
+
+export default Main;
