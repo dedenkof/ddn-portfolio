e8e22d1310c946126579e736ba5278431131106c
Volodymyr Dedenko <dedenkof@ukr.net>
2019-07-09 18:51:52 +0300

    Add social links block to landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,11 +6,34 @@ function Landing() {
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
                     <Cell col={12} tablet={12} phone={12}>
-                        <img className={"avatar"} src={"https://i.stack.imgur.com/lxUOp.png?s=328&g=1"} alt={"avatar"}/>
+                        <img className="avatar" src="https://i.stack.imgur.com/lxUOp.png?s=328&g=1" alt="avatar"/>
                         <div className="avatar-text">
                             <h2>Full Stack Web Developer</h2>
                             <hr/>
                             <p>Web Designer | HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB | PHP/MySQL</p>
+                            <div className="social-links">
+
+                                {/* LinkedIn */}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* Github */}
+                                <a href="https://github.com/dedenkof" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+                                {/* Freecodecamp */}
+                                <a href="https://github.com/dedenkof" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
+                                </a>
+
+                                {/* Behance */}
+                                <a href="https://github.com/dedenkof" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-behance-square" aria-hidden="true" />
+                                </a>
+
+                            </div>
                         </div>
                     </Cell>
                 </Grid>
